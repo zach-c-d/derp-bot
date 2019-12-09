e1edd8c3c1d2babaa6997155fe27315961d0c875
dylan1313 <dylan@DESKTOP-NR6J0CC.localdomain>
2019-12-09 03:12:03 -0500

    added turncorner internal code.

--- include/actuators.h
+++ include/actuators.h
@@ -1,27 +1,45 @@
 void align_with_line()
 {
-
+	
 }
 
 void move_forward(int motor_speed)
 {
-
+	
 }
 
 void turn_corner(bool turn_left)
 {
   if (turn_left)
   {
-
+	motorFL.run(BACKWARD); motorFR.run(FORWARD); // TURN LEFT
+    motorBL.run(BACKWARD); motorBR.run(FORWARD);
+	delay(100);
+	if (!alined)
+	{
+		turn_corner(turn_left);
+	}else{
+		align_with_line();
+	}
+	
   }
   else
   {
-
+	motorFL.run(FORWARD); motorFR.run(BACKWARD); // TURN RIGHT
+    motorBL.run(FORWARD); motorBR.run(BACKWARD);
+	delay(100);
+	if (!alined)
+	{
+		turn_corner(turn_left);
+	}else{
+		align_with_line();
+	}
+	
   }
 }
 bool break_in_line()
 {
-
+	
 }
 
 bool break_is_t_corner()
