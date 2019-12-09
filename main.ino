3c21fef6f6526530d87473a8652e5544a2cb3c7a
chalmers <zach@chalmerscards.com>
2019-12-09 00:20:52 -0500

    more main.ino additions

--- main.ino
+++ main.ino
@@ -15,20 +15,23 @@ void setup()
 
 void loop()
 {
+  //make sure dis bot is frickin aligned!!!
+  //do not leave this function until you very very sure!
   align_with_line();
   //once bot is aligned with line, move forward one unit
   move_forward(motor_speed);
 
-  //when break
+  //when break in line is found
   if(break_in_line())
   {
     if(break_is_corner())
     {
       turn_corner(last_corner_turn);
     }
+    //if break is not corner, it must be a ball container
     else
     {
-
+      //check to see if there is a candle lit in the ball containment area
     }
   }
 }
